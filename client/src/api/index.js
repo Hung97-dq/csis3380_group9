5e50b86fbc7dba5021a75b89a42b581b803d0be9
Hung Dinh <hung.dq1997hn@gmail.com>
2023-04-16 12:11:42 -0700

    third commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://csis3380-002-group9.herokuapp.com/' });
+const API = axios.create({ baseURL: 'http://localhost:5000/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
